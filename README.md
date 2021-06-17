f4bc45cfef75dbeaa831d9ccb6c884bdb2dafc0b
252009 <84382723+252009@users.noreply.github.com>
2021-06-17 18:02:45 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# geologist-eror
+# Geologist
+Geologist Project
---
+++ sketch.js
@@ -0,0 +1,35 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    Iron = new iron(300,300,30)
+
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    Iron.display();
+
+
+    
+ 
+}
